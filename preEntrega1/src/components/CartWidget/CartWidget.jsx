40a80b33d616d539dbf62e8c7eeafb58d7fea2b6
Santiago Penino <santiago.penino@gmail.com>
2023-08-05 18:54:36 -0300

    navbar estilizada

--- preEntrega1/src/components/CartWidget/CartWidget.jsx
+++ preEntrega1/src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,11 @@
 import { Icon } from '@iconify/react';
+import './cartwidget.css'
 
 const CART_ITEMS = 1;
 const CART_WIDGET = () => {
     return(
         <>
-        <Icon className='cart__icon' icon="raphael:cart" />
+        <Icon className='cart-icon' icon="raphael:cart" />
         {CART_ITEMS}
         </>
     )
